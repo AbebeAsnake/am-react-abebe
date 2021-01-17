2cba1530d1eac045b66819b9c2347980e015e826
Abebe asnake <abexasnex@gmail.com>
2021-01-16 20:49:43 -0500

    label, progress bar and eyeicon added

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import EyeIcon from 'am-react-abebe/EyeIcon';
+
+export default function EyeIconExample() {
+  return <EyeIcon />;
+}
---
+++ src/docs/examples/ProgressBar/Example100Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'am-react-abebe/ProgressBar';
+
+/** 100% progress and height 10px */
+export default function Example10Percent() {
+  return <ProgressBar percent={100} width={150}  height={10} />
+}
